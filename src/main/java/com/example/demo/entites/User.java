2d361dedaa58eb354104f8475e69d9a66d5f8e2f
MateusAntony <mateus.medeiros110@gmail.com>
2025-01-23 14:14:28 -0300

    Product e Category

--- src/main/java/com/example/demo/entites/User.java
+++ src/main/java/com/example/demo/entites/User.java
@@ -12,6 +12,9 @@ import java.util.List;
 @Table(name="tb_user")
 public class User implements Serializable {
 
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
